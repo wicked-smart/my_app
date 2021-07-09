305405a05fdd20ac9c545e85da2b2384db1e8c8e
wicked-smart <marzaanvan@gmail.com>
2021-07-09 18:25:47 +0530

    Modify main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(
       primarySwatch: Colors.purple,
@@ -15,17 +16,50 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('OTP Validation!'),
+        title: Text('Verify E-mail'),
       ),
       body: Column(
         children: [
           Image.asset(
             "assets/images/hey-logo.png",
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
+            width: 700,
+            semanticLabel: 'hey\'s logo',
+            //color: Colors.teal,
+            //colorBlendMode: BlendMode.darken,
+            //height: 400,
           ),
           SizedBox(
             height: 20.0,
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  //border: OutlineInputBorder(),
+                  labelText: "E-mail",
+                  hintText: 'Enter your e-mail id',
+                  suffixIcon: TextButton(
+                    child: Text("Send OTP"),
+                    onPressed: () {},
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                //border: OutlineInputBorder(),
+                hintText: 'Your OTP',
+                labelText: "OTP",
+              ),
+            ),
+          ),
+          ElevatedButton(
+              //style: sty,
+              onPressed: () {},
+              child: const Text('Verify OTP')),
         ],
       ),
     );
